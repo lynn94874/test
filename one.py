39d89314ceb3f233bfdd5bf4addc59833a94190e
lynn94874 <lynn94874@gmail.com>
2021-01-21 15:48:02 +0800

    11

--- one.py
+++ one.py
@@ -1,61 +1,16 @@
-import cmath
-import random
-import init
-init.Ini()
+import numpy as np
 
-'''
-a = float(input( ))
-b = float(input( ))
-a = cmath.sqrt(a)
-b = cmath.sqrt(b)
-print("a = {:.2f}, b = {:.1f}".format(a, b))
+y = np.array([-9, 2, 2, 5, 8, 8])
 
-a = random.randint(2, 4)
-b = random.randint(2,10000)
-print("{:} {:}".format(a, b))
+yy = np.array([2, 2, 3, 5, 7, 8])
 
-a = int(input())
-b = int(input())
-c = a**b
-d = a%b
-print('哈哈哈是 %d, 嘿嘿嘿是 %d' %(c, d))
+x = np.array([[1, 2, 0], [4, 5, 6], [7, 8, 9], [1, 2, 3]])
 
-a = input()
-if init.Is_num(a)==True:
-    print("这是一个数字")
-else :
-    print("这不是一个数字")
+y = np.array([[1, 2, 0], [-4, 5, 6], [7, 8, 9]])
 
-a = int(input())
-ans = 1
-for i in range(1,a+1):
-    ans *= i
-print("{:}".format(ans))
-
-for i in range(1, 10):
-    for j in range(1, i+1):
-        print("{}*{}={}\t".format(i, j, i*j), end='')
-    print()
+ID = np.array([1, 2, 3])
 
 
-class One():
-
-    a = ''
-
-    def __init__(self, aa):
-
-        self.a = aa
-
-    def yaya(self):
-        print("%s" %(self.a))
 
 
-name = input()
-a = One(name)
-a.yaya()
-
-'''
-
-
-A = [x ** 2 for x in range(a, b+1)]
-b = 12
+print(x[ID])
