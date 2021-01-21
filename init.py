ee4d2c72b5ad7ff28ed2491dc00f72acdc2ff1f2
lynn94874 <lynn94874@gmail.com>
2021-01-21 15:43:27 +0800

    11

--- init.py
+++ init.py
@@ -7,17 +7,3 @@ def Ini():
     sys.path.append("D:\testcode")
 
 
-def Is_num(a):
-
-    try:
-        float(a)
-        return True
-    except ValueError:
-        pass
-
-    try:
-        unicodedata.numeric(a)
-        return True
-    except (ValueError, TypeError):
-        pass
-    return False
